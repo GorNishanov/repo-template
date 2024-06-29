acba1add67ddfd1862b0fb7695bff0f58b9786db
GorNishanov <GorNishanov@gmail.com>
2024-06-29 09:21:04 -0500

    template ready

---
+++ src/conqueue.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2023 Gor Nishanov
+// Licensed under MIT license. See LICENSE.txt for details.
+
+#include <std/experimental/conqueue>
---
+++ test/conqueue.test.cpp
@@ -0,0 +1,8 @@
+#include <catch2/catch_test_macros.hpp>
+#include <print>
+
+using namespace std;
+TEST_CASE("conqueue: smoketest") {
+  println("Hello");
+  REQUIRE(true);
+}
